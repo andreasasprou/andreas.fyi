ceea2cd9810f8b62ad505e0c549623a919356577
Andreas Asprou <andyasprou@gmail.com>
2021-12-27 15:29:53 +0000

    fix(Code): font size on small devices

--- components/notion/Code.tsx
+++ components/notion/Code.tsx
@@ -5,30 +5,6 @@ import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-typescript';
 
-// export const Code = ({
-//                        code,
-//                        language = 'javascript',
-//                      }: {
-//   code: string;
-//   language: string;
-// }) => {
-//   const languageL = language.toLowerCase();
-//   const prismLanguage = languages[languageL] || languages.javascript;
-//
-//   console.log(code);
-//
-//   return (
-//     <pre className="notion-code">
-//       <code
-//         className={`language-${languageL}`}
-//         dangerouslySetInnerHTML={{
-//           __html: highlight(code, prismLanguage, language),
-//         }}
-//       />
-//     </pre>
-//   );
-// };
-
 export const Code = ({
   children,
   language = 'javascript',
@@ -38,7 +14,7 @@ export const Code = ({
 }) => {
   return (
     <>
-      <pre className="bg-gray-900 p-2 md:p-4 mb-4 md:mb-8 overflow-x-auto">
+      <pre className="text-sm md:text-base bg-gray-900 p-2 md:p-4 mb-4 md:mb-8 overflow-x-auto">
         <code
           dangerouslySetInnerHTML={{
             __html: Prism.highlight(
