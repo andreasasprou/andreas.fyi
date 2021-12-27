e034cba25cc0e924b1cb95f8732f6badf061efa2
Andreas Asprou <andyasprou@gmail.com>
2021-12-27 10:36:24 +0000

    imp(SEO): add redirects from old pages

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@
 module.exports = {
   reactStrictMode: true,
   optimizeFonts: false,
+  redirects: function () {
+    return [
+      {
+        source: '/engineering/nextjs-auth-skeleton-loaders',
+        destination: '/blog/nextjs-auth-skeleton-loaders',
+        permanent: true
+      },
+      {
+        source: '/thinking/early-stage-founder-character',
+        destination: '/blog/early-stage-founder-character',
+        permanent: true
+      }
+    ]
+  }
 }
