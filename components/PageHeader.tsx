dbf9329f3d1472296dc064519aa73d0d9821af2b
Andreas Asprou <andyasprou@gmail.com>
2021-12-27 15:28:49 +0000

    fix(Headings): small device styles

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -16,10 +16,12 @@ export function PageHeader({
 }: PageHeaderProps) {
   return (
     <div className="border-b border-b-white/20 pb-4 mb-4 md:pb-8 md:mb-8">
-      <h1 className="md:text-6xl text-4xl font-bold text-white/90 leading-normal">
+      <h1 className="md:text-6xl text-3xl font-bold text-white/90 leading-normal">
         {title}
       </h1>
-      {subTitle && <p className="mt-6 text-xl text-white/80">{subTitle}</p>}
+      {subTitle && (
+        <p className="mt-3 md:mt-6 text-xl text-white/80">{subTitle}</p>
+      )}
       {createdAt && (
         <p className="text-white/70 mt-4">
           {dayjs(createdAt).format('MMM D, YYYY')}
--- components/notion/NotionHeading.tsx
+++ components/notion/NotionHeading.tsx
@@ -12,15 +12,18 @@ interface HeadingConfig {
 
 const headingConfig: Record<HeadingBlock['type'], HeadingConfig> = {
   heading_1: {
-    classes: 'text-4xl font-bold leading-snug mt-16 mb-4 text-white/90',
+    classes:
+      'text-2xl md:text-4xl font-bold leading-snug mt-8 md:mt-16 mb-2 md:mb-4 text-white/90',
     as: 'h1',
   },
   heading_2: {
-    classes: 'text-3xl font-bold mt-12 mb-4 text-white/90',
+    classes:
+      'text-xl md:text-3xl font-bold mt-6 md:mt-12 mb-2 md:mb-4 text-white/90',
     as: 'h2',
   },
   heading_3: {
-    classes: 'text-2xl font-bold mt-8 mb-2 text-white/90',
+    classes:
+      'text-lg md:text-2xl font-bold mt-4 md:mt-8 mb-1 md:mb-2 text-white/90',
     as: 'h3',
   },
 };
