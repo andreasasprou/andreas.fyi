7e06c450cf6fb43805c88ae70ef24682512eb70c
Andreas Asprou <andyasprou@gmail.com>
2022-06-06 16:52:14 +0100

    imp(tracking): replace simpleanalytics with ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,15 +42,32 @@ class MyDocument extends Document {
             <>
               <script
                 async
-                defer
-                src="https://scripts.simpleanalyticscdn.com/latest.js"
-              ></script>
-              <noscript>
-                <img
-                  src="https://queue.simpleanalyticscdn.com/noscript.gif"
-                  alt=""
-                />
-              </noscript>
+                src="https://www.googletagmanager.com/gtag/js?id=G-HKF3WQS94Y"
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HKF3WQS94Y');`,
+                }}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3006162,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');    
+                  `,
+                }}
+              />
             </>
           )}
         </Head>
